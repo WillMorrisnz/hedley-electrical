aa6430f6294510a0add8b9eca76d4cf264d00a85
William Morris <ewm27@uclive.ac.nz>
2022-01-04 20:17:45 +1300

    Nav changes  - Made nav content dynamic pulling from routes list  - Fixed bug, previous page buttons would keep nav menu open on mobile

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,33 +1,54 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import hamburger from '../assets/images/hamburger.svg'
 
 
 
 const Nav = () => {
     const [mobileTabOpen, setMobileTabOpen] = useState(false);
-    const [selectedTab, setSelectedTab] = useState(0)
+    const [location, setLocation] = useState(0)
+    
+    const routes = [
+        {
+            path: '/',
+            name: 'Home'
+        },
+        {
+            path: '/services',
+            name: 'Services'
+        },
+        {
+            path: '/about',
+            name: 'About'
+        },
+        {
+            path: '/contact',
+            name: 'Contact'
+        },
+    ]
 
     // Toggles the mobile menu overlay
     const toggleMobileMenu = () => {
         setMobileTabOpen(!mobileTabOpen)
     }
 
-    //  Sets the selected tab index for active highlighting and 
-    //  knowing when to close the mobile menu window
-    const tabSelected = (index) => {
-        setSelectedTab(index)
-        toggleMobileMenu()
-    }
+    //Manages the back button functionality, so if a user has the mobile menu
+    //open and goes back the menu is closed and updated correctly
+    const routelocation = useLocation()
+    if (routelocation.pathname != location) {
+        setLocation(routelocation.pathname)
+        setMobileTabOpen(false)
+    } 
 
     return(
         <div className='nav-container'>
             <img src={hamburger} onClick={toggleMobileMenu} className="mobile-nav-button"></img>
             <nav hidden={!mobileTabOpen} data-visible={mobileTabOpen}>
-                <Link onClick={() => {tabSelected(0)}} className={selectedTab === 0 ? 'nav-item-selected' : 'nav-item'} to={'/'}>Home</Link>
-                <Link onClick={() => {tabSelected(1)}} className={selectedTab === 1 ? 'nav-item-selected' : 'nav-item'} to={'/services'}>Services</Link>
-                <Link onClick={() => {tabSelected(2)}} className={selectedTab === 2 ? 'nav-item-selected' : 'nav-item'} to={'/about'}>About</Link>
-                <Link onClick={() => {tabSelected(3)}} className={selectedTab === 3 ? 'nav-item-selected' : 'nav-item'} to={'/contact'}>Contact</Link>
+                {routes.map((route) => {
+                    return (
+                        <Link key={route.path} className={location === route.path ? 'nav-item-selected' : 'nav-item'} to={route.path}>{route.name}</Link>
+                    )
+                })}
             </nav>
         </div>
     )
