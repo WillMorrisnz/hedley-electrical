11d5250feac58efb414e7715864d50dc60922d46
William Morris <ewm27@uclive.ac.nz>
2022-01-10 16:26:29 +1300

    Created grid cards with images

--- src/pages/servicesPage.js
+++ src/pages/servicesPage.js
@@ -1,16 +1,31 @@
 import React from 'react';
+import image from '../assets/images/image0.png'
+import image1 from '../assets/images/image1.png'
+import image2 from '../assets/images/image2.jpeg'
 
 const ServicesPage = () => {
     return (
         <section className='template-grid'>
-            <div className='card-outline '>
-                <h4>Residential</h4>
+            <div className='card-outline stacked'>
+                <img className='card__img' src={image}></img>
+                <div className='blue_gradient'></div>
+                <div className='card__content'>
+                    <h4 className='lemon'>Residential</h4>
+                </div>
             </div>
-            <div className='card-outline '>
-                <h4>Commercial</h4>
+            <div className='card-outline stacked'>
+                <img className='card__img' src={image1}></img>
+                <div className='blue_gradient'></div>
+                <div className='card__content'>
+                    <h4 className='lemon'>Commercial</h4>
+                </div>
             </div>
-            <div className='card-outline '>
-                <h4>Gate Automation</h4>
+            <div className='card-outline stacked'>
+                <img className='card__img' src={image2}></img>
+                <div className='blue_gradient'></div>
+                <div className='card__content'>
+                    <h4 className='lemon'>Gate Automation</h4>
+                </div>
             </div>
         </section>
     )
