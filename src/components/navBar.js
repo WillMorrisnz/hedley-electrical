6ad9630acc7ef44ea7daae5c05fdcbcaa097c6ad
William Morris <ewm27@uclive.ac.nz>
2022-01-10 20:13:25 +1300

    Nav changes  - Nav close button  - Nav icon changes

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import hamburger from '../assets/images/hamburger.svg'
+import hamburgerIcon from '../assets/images/hamburger.svg'
+import exitIcon from '../assets/images/exit.svg'
 
 
 
@@ -41,16 +42,21 @@ const Nav = () => {
     } 
 
     return(
-        <div className='nav-container'>
-            <img src={hamburger} onClick={toggleMobileMenu} className="mobile-nav-button"></img>
-            <nav hidden={!mobileTabOpen} data-visible={mobileTabOpen}>
-                {routes.map((route) => {
-                    return (
-                        <Link key={route.path} className={location === route.path ? 'nav-item-selected' : 'nav-item'} to={route.path}>{route.name}</Link>
-                    )
-                })}
-            </nav>
+        <div>
+            <div className='nav-container'>
+                <img src={mobileTabOpen ? exitIcon : hamburgerIcon} onClick={toggleMobileMenu} className="mobile-nav-button"></img>
+                <nav hidden={!mobileTabOpen} data-visible={mobileTabOpen}>
+                    {routes.map((route) => {
+                        return (
+                            <Link key={route.path} className={location === route.path ? 'nav-item-selected' : 'nav-item'} to={route.path}>{route.name}</Link>
+                        )
+                    })}
+                </nav>
+            </div>
+            {/* Close nav button */}
+            <div onClick={toggleMobileMenu} className="close-nav"  hidden={!mobileTabOpen} data-visible={mobileTabOpen}></div>
         </div>
+
     )
 }
 
